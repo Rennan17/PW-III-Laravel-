ab4a76a238e235c2013e718ae9405813f500c56a
Rennan17 <rsmoura252@gmail.com>
2022-08-07 21:06:20 -0300

    First activity - menu (nav-bar)

--- projeto-base/routes/web.php
+++ projeto-base/routes/web.php
@@ -12,7 +12,22 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
+});
+//Route é um classe, que esta sendo acessado pelo metodo get, que está recebendo dois parametros
+Route::get('/cliente', function () {   
+    return view('cliente');
+});
+
+Route::get('/categoria', function () {   
+    return view('categoria');
+});
+
+Route::get('/produto', function () {   
+    return view('produto');
+});
+
+Route::get('/pedido', function () {   
+    return view('pedido');
 });
