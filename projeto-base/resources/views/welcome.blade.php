878ac796aa420d195577849f268be16100736a1a
Rennan17 <rsmoura252@gmail.com>
2022-08-09 11:17:43 -0300

    criação da pagina incial

--- projeto-base/resources/views/welcome.blade.php
+++ projeto-base/resources/views/welcome.blade.php
@@ -5,7 +5,12 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
+        <link rel="stylesheet" href="{{url('css/reset.min.css')}}">
+        <link rel="stylesheet" href="{{url('css/default.css')}}">
+        <link rel="stylesheet" href="{{url('css/styleCar.css')}}">
+        <link rel="stylesheet" href="{{url('css/animation.css')}}">
         <link rel="stylesheet" href="{{url('css/style.css')}}">
+
         <link
             rel="stylesheet"
             href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
@@ -15,45 +20,74 @@
             src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
             integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
             crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="{{url('css/style.css')}}">
+
     </head>
-
     <body>
-        <nav class="navbar navbar-light bg-light">
-            <a class="navbar-brand" href="#">
-                <img
-                    src="img/smoke.jpg"
-                    width="50"
-                    height="50"
-                    class="d-inline-block align-top"
-                    alt="">
-                Empresa "X"
-            </a>
 
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <body>
+            <nav class="navbar navbar-light bg-light">
+                <a class="navbar-brand" href="/">
+                    <img
+                        src="img/smoke.jpg"
+                        width="50"
+                        height="50"
+                        class="d-inline-block align-top"
+                        alt="">
+                    Empresa "X"
+                </a>
 
-                <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarNavAltMarkup"
-                    aria-controls="navbarNavAltMarkup"
-                    aria-expanded="false"
-                    aria-label="Alterna navegação">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-item nav-link active" href="/cliente">Cliente
-                            <span class="sr-only">(Página atual)</span></a>
-                        <a class="nav-item nav-link active" href="/categoria">Categoria
-                            <span class="sr-only">(Página atual)</span></a>
-                        <a class="nav-item nav-link active" href="/produto">Produto
-                            <span class="sr-only">(Página atual)</span></a>
-                        <a class="nav-item nav-link active" href="/pedido">Pedido
-                            <span class="sr-only">(Página atual)</span></a>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+                    <button
+                        class="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarNavAltMarkup"
+                        aria-controls="navbarNavAltMarkup"
+                        aria-expanded="false"
+                        aria-label="Alterna navegação">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <a class="nav-item nav-link active" href="/cliente">Cliente
+                                <span class="sr-only">(Página atual)</span></a>
+                            <a class="nav-item nav-link active" href="/categoria">Categoria
+                                <span class="sr-only">(Página atual)</span></a>
+                            <a class="nav-item nav-link active" href="/produto">Produto
+                                <span class="sr-only">(Página atual)</span></a>
+                            <a class="nav-item nav-link active" href="/pedido">Pedido
+                                <span class="sr-only">(Página atual)</span></a>
+                        </div>
                     </div>
-                </div>
+                </nav>
             </nav>
+
+            <div class="container">
+                <main class="main">
+                    <section class="galery">
+                        <div class="fotos">
+                            <img src="./img/cadastro.jpg" alt="Foto 1">
+                            <img src="./img/about-us.jpg" alt="Foto 2">
+                            <img src="./img/cadastro.jpg" alt="Foto 1">
+                            <img src="./img/about-us.jpg" alt="Foto 2">
+                            <img src="./img/cadastro.jpg" alt="Foto 1">
+                        </div>
+                    </section>
+                </main>
+             
+            </div>
+
+               
+            <footer>
+                    <p>Desenvolvido por: Rennan Moura<br>
+                        <a href="mailto:rsmoura252@gmail.com">rsmoura252@gmail.com</a>
+                    </p>
+                </footer>
+
+            <a href="/">Home</a>
+            <a href="contato">Contato</a>
 
         </body>
     </html>
